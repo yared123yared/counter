0d0a285b4fb765511a0044b96562acbea2e42b3f
=yared123yared <=yaredyaya16@gmail.com>
2020-10-08 04:50:20 +0300

    initial

--- src/Components/counter.jsx
+++ src/Components/counter.jsx
@@ -3,13 +3,13 @@ class Counter extends Component {
     
 
     render() {
-        
-       console.log("countered-rendered");
+        // 
+    //    console.log("countered-rendered");
            
     
             return ( 
                  <div>
-               
+               {/*  */}
                     <span style = { { fonstSize: 10, fontWeight: 'bold' }}className = { this.getBadgeClasses() } > { this.formatCount() } </span> 
                     <button onClick={()=>this.props.onIncrement(this.props.counter)} className = "btn btn-secondary btn-sm" > Increment </button >
                     <button onClick={()=> this.props.onDelete(this.props.counter)} className="btn btn-danger btn-sm m-2">Delete</button>
